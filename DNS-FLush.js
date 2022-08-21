74a611b9f6da8f60d53d5df6a91d4955e6224546
jianoliu <liujian19781028@gmail.com>
2022-08-21 10:59:33 +0800

    Create DNS-FLush.js

---
+++ DNS-FLush.js
@@ -0,0 +1,34 @@
+/*
+ * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+ * 更新日期：2022.08.21
+ * 版本：1.8
+ */
+
+!(async () => {
+    let panel = { title: "Flush DNS" },
+        showServer = true,
+        dnsCache;
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        if (arg.title) panel.title = arg.title;
+        if (arg.icon) panel.icon = arg.icon;
+        if (arg.color) panel["icon-color"] = arg.color;
+        if (arg.server == "false") showServer = false;
+    }
+    if (showServer) {
+        dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
+        dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
+    }
+    if ($trigger == "button") await httpAPI("/v1/dns/flush");
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    panel.content = `DNS延迟: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`;
+    $done(panel);
+})();
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
